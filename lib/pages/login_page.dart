bcafd89c0d61678a41046891b4770e90cc5b71ed
Ibrahim <savadogoibrahim0915@gmail.com>
2026-04-04 21:09:04 +0000

    second tentative de login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -23,78 +23,40 @@ class _LoginPageState extends State<LoginPage> {
               onPressed: () => Navigator.pop(context),
             ),
           ),
+          Positioned(
+              top: 80,
+               right: 50,
+              left: 50,
+              child:   Column(
+                children: [
+                  Text("Welcome to the login page",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Please Enter yours !")
+                ],
+              )
+          ),
+
+
+          Positioned(
+                top:  250,
+                left: 0,
+                right: 0,
+                bottom: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20)
+                ),
+
+              )
+          )
 
           // 2. LES CHAMPS (Centrés proprement)
-          Center( // Utilise Center au lieu d'une Column seule dans un Stack
-            child: Container(
-              width: 320, // Un peu plus large pour le confort
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)], // Optionnel: pour voir le cadre
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min, // Important: le container s'adapte au contenu
-                children: [
-                  // CHAMP EMAIL
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      prefixIcon: const Icon(Icons.person),
-                      // Bordure au repos
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(40),
-                        borderSide: const BorderSide(width: 3, color: Colors.blue),
-                      ),
-                      // Bordure quand on clique (C'est ça qui manquait !)
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(40),
-                        borderSide: const BorderSide(width: 3, color: Colors.blueAccent),
-                      ),
-                    ),
-                  ),
 
-                  const SizedBox(height: 20),
-
-                  // CHAMP MOT DE PASSE
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Mot de passe",
-                      prefixIcon: const Icon(Icons.lock),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(width: 3, color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(width: 3, color: Colors.blue),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 8,),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("Tentative de connexion...");
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      minimumSize: const Size(double.infinity, 50), // Prend toute la largeur
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    child: const Text(
-                      "SE CONNECTER",
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
         ],
       ),
     );
