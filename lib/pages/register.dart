4a26b8ba440ae589ce7cf4543d0b622b14d517fc
Ibrahim <savadogoibrahim0915@gmail.com>
2026-04-05 12:07:35 +0000

    debit register

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -11,8 +11,23 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
+      body: Padding(padding: EdgeInsets.only(top: 25, left: 0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              IconButton(
+               onPressed:()=>Navigator.pop(context),
+          icon: Icon(Icons.arrow_back_ios,
+          size: 30,
+          color: Colors.white,)),
+            ],
+          )
 
+        ],
+      ),
+      )
     );
   }
 }
